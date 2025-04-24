1874288240351540305d3327a03ea0804026f467
Your Name <your.email@example.com>
2025-04-24 12:53:19 -0700

    Cleanup: Remove unnecessary migration scripts

--- update-imports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Find and replace in all code files
-find ./docs ./examples ./src -type f -name "*.ts" -o -name "*.tsx" -o -name "*.js" -o -name "*.jsx" | xargs sed -i '' "s/@vibing\/block-kit/@vibing-ai\/block-kit/g"
-
-echo "Imports updated from '@vibing/block-kit' to '@vibing-ai/block-kit'" 
